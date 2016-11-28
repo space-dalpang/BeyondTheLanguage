e655305bbeb9ba6612f2b31d8fd2f90e7e83b41f
Nesswit <rishubil@gmail.com>
2016-11-29 03:45:37 +0900

    Add retries to api call for fix windows docker network issue

--- WordToShape/Assets/Scripts/Api.cs
+++ WordToShape/Assets/Scripts/Api.cs
@@ -15,17 +15,43 @@ public class Api : MonoBehaviour
 
 	private IEnumerator InternalGetSenti (string query, OnGetSenti cb, OnErrorSenti errorCb)
 	{
-		string host = "192.168.99.100:8080";
+		string host = "192.168.99.100";
 //		string host = "localhost";
 		string url = "http://" + host + "/senti?q=" + WWW.EscapeURL(query);
-		WWW www = new WWW (url);
-		yield return www;
-		if (www.error == null) {
-			JSONObject jo = new JSONObject (www.text);
-			cb (jo);
-		} else {
-			errorCb (url + ": " + www.error);
-			Debug.Log ("WWW Error: " + www.error);
+
+		int retryCount = 0;
+		while (retryCount < 10) {
+			WWW www = new WWW (url);
+
+			float timer = 0; 
+			bool failed = false;
+			float timeOut = 1;
+
+			while (!www.isDone) {
+				Debug.Log (timer);
+				if (timer > timeOut) {
+					failed = true;
+					break;
+				}
+				timer += Time.deltaTime;
+				yield return null;
+			}
+			if (failed) {
+				www.Dispose ();
+				retryCount++;
+				continue;
+			}
+
+			yield return www;
+
+			if (www.error == null) {
+				JSONObject jo = new JSONObject (www.text);
+				cb (jo);
+			} else {
+				errorCb (url + ": " + www.error);
+				Debug.Log ("WWW Error: " + www.error);
+			}
+			break;
 		}
 
 	}
